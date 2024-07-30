11a4181bbf2d1876cc752da01407073f1108e3e4
phfr24 <phfr24@inf.ufpr.br>
2024-07-30 15:14:22 -0300

    refactor question/ and add questionData

---
+++ src/data/questionData.js
@@ -0,0 +1,115 @@
+const questionData = [
+  {
+    question: "O que gostaria de fazer nas horas vagas?",
+    options:
+      [
+        "Apostar em corridas de cavalo",
+        "Esquiar",
+        "Mergulhar",
+        "Tentar descobrir como meu despertador funciona",
+        "Ir à missa"
+      ]
+  },
+  { 
+    question: "Se fosse para você escolher uma segunda faculdade, qual seria?",
+    options:
+      [
+        "Matemática",
+        "Farmácia",
+        "Artes",
+        "Matemática",
+        "Teologia"
+      ]
+  },
+  { 
+    question: "Qual área da computação mais te interessa?",
+    options:
+      [
+        "Arquitetura de computadores",
+        "Algoritmos",
+        "Desenvolvimento de jogos",
+        "Otimização",
+        "Codificação"
+      ]
+  },
+  { 
+    question: "Qual desses projetos você participaria?",
+    options:
+      [
+        "Criação de um algoritmo que permite uma máquina analítica a computar valores de funções matemáticas",
+        "Desenvolvimento do software Centaur para o processo conhecido como \"staging\" - processo de combinação de várias sessões de foguetes que pegam fogo em uma ordem específica e, então, se destacam da nave principal, para que essa atinja o espaço",
+        "Desenvolvimento de um jogo projetado em torno de storytelling, exploração aberta e ensaiar cenários realistas do dia a dia, em oposição a jogos competitivos com pontuações e segmentos cronometrados, destinado a meninas entre 8 e 14 anos",
+        "Desenvolvimento de um compilador que converte termos em inglês para código de máquina",
+        "Desenvolvimento de uma nova linguagem de programação que traduz os códigos dos computadores de maneira mais direta e tem como propósito ampliar a programação de computadores em campos não matemáticos e científicos, o que permitiria que qualquer pessoa pudesse aprender a linguagem, tornando-a mais acessível"
+      ]
+  },
+  { 
+    question: "Qual dessas causas você mais se relaciona?",
+    options:
+      [
+        "Computador como uma máquina multifuncional que pode servir para todas as áreas",
+        "Inclusão de minorias sociais na STEM (ciência, tecnologia, engenharia e matemática)",
+        "Diversidade e inclusão na indústria dos games",
+        "Informática mais acessível a todos os públicos",
+        "O computador pode auxiliar na democratização da informação para a educação de qualidade"
+      ]
+  },
+  
+  {
+    question: "Qual dessas frases seria mais provável você dizer em uma roda de amigos?",
+    options:
+      [
+        "\“É a imaginação que penetra nos mundos invisíveis que nos rodeiam, nos mundos da ciência.\ˮ",
+        "\“Se eu não posso trabalhar com você, vou trabalhar no mesmo ambiente que você.\ˮ",
+        "\“Você não precisa agradar a todos, precisa agradar o usuário.\ˮ",
+        "\“Se eles te colocarem em algum lugar sem nada para fazer, vá dormir - você não sabe quando vai conseguir mais.\ˮ ou \“Tem um bug nesse sistema.\ˮ",
+        "\“É óbvio que a informação é inútil se ninguém tiver acesso a ela.\ˮ"
+      ]
+  },
+  { 
+    question: "",
+    options:
+      [
+        "",
+        "",
+        "",
+        "",
+        ""
+      ]
+  },
+  { 
+    question: "",
+    options:
+      [
+        "",
+        "",
+        "",
+        "",
+        ""
+      ]
+  },
+  { 
+    question: "",
+    options:
+      [
+        "",
+        "",
+        "",
+        "",
+        ""
+      ]
+  },
+  { 
+    question: "",
+    options:
+      [
+        "",
+        "",
+        "",
+        "",
+        ""
+      ]
+  }
+];
+  
+  export default questionData;
