005a7f6cbfe8d5c7ce5fcbea92279460379e7075
phfr24 <phfr24@inf.ufpr.br>
2024-07-31 10:48:00 -0300

    map answers

--- src/data/questionData.js
+++ src/data/questionData.js
@@ -1,115 +1,149 @@
 const questionData = [
   {
     question: "O que gostaria de fazer nas horas vagas?",
-    options:
-      [
-        "Apostar em corridas de cavalo",
-        "Esquiar",
-        "Mergulhar",
-        "Tentar descobrir como meu despertador funciona",
-        "Ir à missa"
-      ]
+    options: [
+      ["Apostar em corridas de cavalo", "ada"],
+      ["Esquiar", "annie"],
+      ["Mergulhar", "brenda"],
+      ["Tentar descobrir como meu despertador funciona", "grace"],
+      ["Ir à missa", "mary"],
+    ],
   },
   {
     question: "Se fosse para você escolher uma segunda faculdade, qual seria?",
-    options:
-      [
-        "Matemática",
-        "Farmácia",
-        "Artes",
-        "Matemática",
-        "Teologia"
-      ]
+    options: [
+      ["Matemática", "ada"],
+      ["Farmácia", "annie"],
+      ["Artes", "brenda"],
+      ["Matemática", "grace"],
+      ["Teologia", "mary"],
+    ],
   },
   {
     question: "Qual área da computação mais te interessa?",
-    options:
-      [
-        "Arquitetura de computadores",
-        "Algoritmos",
-        "Desenvolvimento de jogos",
-        "Otimização",
-        "Codificação"
-      ]
+    options: [
+      ["Arquitetura de computadores", "ada"],
+      ["Algoritmos", "annie"],
+      ["Desenvolvimento de jogos", "brenda"],
+      ["Otimização", "grace"],
+      ["Codificação", "mary"],
+    ],
   },
   {
     question: "Qual desses projetos você participaria?",
-    options:
+    options: [
       [
         "Criação de um algoritmo que permite uma máquina analítica a computar valores de funções matemáticas",
+        "ada",
+      ],
+      [
         "Desenvolvimento do software Centaur para o processo conhecido como \"staging\" - processo de combinação de várias sessões de foguetes que pegam fogo em uma ordem específica e, então, se destacam da nave principal, para que essa atinja o espaço",
+        "annie",
+      ],
+      [
         "Desenvolvimento de um jogo projetado em torno de storytelling, exploração aberta e ensaiar cenários realistas do dia a dia, em oposição a jogos competitivos com pontuações e segmentos cronometrados, destinado a meninas entre 8 e 14 anos",
+        "brenda",
+      ],
+      [
         "Desenvolvimento de um compilador que converte termos em inglês para código de máquina",
-        "Desenvolvimento de uma nova linguagem de programação que traduz os códigos dos computadores de maneira mais direta e tem como propósito ampliar a programação de computadores em campos não matemáticos e científicos, o que permitiria que qualquer pessoa pudesse aprender a linguagem, tornando-a mais acessível"
-      ]
+        "grace",
+      ],
+      [
+        "Desenvolvimento de uma nova linguagem de programação que traduz os códigos dos computadores de maneira mais direta e tem como propósito ampliar a programação de computadores em campos não matemáticos e científicos, o que permitiria que qualquer pessoa pudesse aprender a linguagem, tornando-a mais acessível",
+        "mary",
+      ],
+    ],
   },
   {
     question: "Qual dessas causas você mais se relaciona?",
-    options:
+    options: [
       [
         "Computador como uma máquina multifuncional que pode servir para todas as áreas",
+        "ada",
+      ],
+      [
         "Inclusão de minorias sociais na STEM (ciência, tecnologia, engenharia e matemática)",
+        "annie",
+      ],
+      [
         "Diversidade e inclusão na indústria dos games",
+        "brenda",
+      ],
+      [
         "Informática mais acessível a todos os públicos",
-        "O computador pode auxiliar na democratização da informação para a educação de qualidade"
-      ]
+        "grace",
+      ],
+      [
+        "O computador pode auxiliar na democratização da informação para a educação de qualidade",
+        "mary",
+      ],
+    ],
   },
-
   {
     question: "Qual dessas frases seria mais provável você dizer em uma roda de amigos?",
-    options:
+    options: [
       [
-        "\“É a imaginação que penetra nos mundos invisíveis que nos rodeiam, nos mundos da ciência.\ˮ",
-        "\“Se eu não posso trabalhar com você, vou trabalhar no mesmo ambiente que você.\ˮ",
-        "\“Você não precisa agradar a todos, precisa agradar o usuário.\ˮ",
-        "\“Se eles te colocarem em algum lugar sem nada para fazer, vá dormir - você não sabe quando vai conseguir mais.\ˮ ou \“Tem um bug nesse sistema.\ˮ",
-        "\“É óbvio que a informação é inútil se ninguém tiver acesso a ela.\ˮ"
-      ]
+        "\"É a imaginação que penetra nos mundos invisíveis que nos rodeiam, nos mundos da ciência.\"",
+        "ada",
+      ],
+      [
+        "\"Se eu não posso trabalhar com você, vou trabalhar no mesmo ambiente que você.\"",
+        "annie",
+      ],
+      [
+        "\"Você não precisa agradar a todos, precisa agradar o usuário.\"",
+        "brenda",
+      ],
+      [
+        "\"Se eles te colocarem em algum lugar sem nada para fazer, vá dormir - você não sabe quando vai conseguir mais.\" ou \"Tem um bug nesse sistema.\"",
+        "grace",
+      ],
+      [
+        "\"É óbvio que a informação é inútil se ninguém tiver acesso a ela.\"",
+        "mary",
+      ],
+    ],
   },
   {
     question: "Qual o seu chá favorito?",
-    options:
-      [
-        "Camomila",
-        "Chá verde",
-        "Chá de hortelã",
-        "Chá preto",
-        "Chá de erva-doce"
-      ]
+    options: [
+      ["Camomila", "ada"],
+      ["Chá verde", "annie"],
+      ["Chá de hortelã", "brenda"],
+      ["Chá preto", "grace"],
+      ["Chá de erva-doce", "mary"],
+    ],
   },
   {
     question: "Que tipo de música você mais gosta?",
-    options:
-      [
-        "Pop",
-        "Eletrônica",
-        "Jazz",
-        "Clássica",
-        "Gospel"
-      ]
+    options: [
+      ["Pop", "ada"],
+      ["Eletrônica", "annie"],
+      ["Jazz", "brenda"],
+      ["Clássica", "grace"],
+      ["Gospel", "mary"],
+    ],
   },
   {
     question: "Qual desses filmes você mais gosta?",
-    options:
-      [
-        "O Jogo da Imitação",
-        "Estrelas Além do Tempo",
-        "Ready Player One",
-        "A Rede Social",
-        "A Teoria de Tudo"
-      ]
+    options: [
+      ["O Jogo da Imitação", "ada"],
+      ["Estrelas Além do Tempo", "annie"],
+      ["Ready Player One", "brenda"],
+      ["A Rede Social", "grace"],
+      ["A Teoria de Tudo", "mary"],
+    ],
   },
   {
     question: "Qual é a sua cor favorita?",
-    options:
-      [
-        "Azul",
-        "Verde",
-        "Amarelo",
-        "Preto",
-        "Branco"
-      ]
-  }
+    options: [
+      ["Azul", "ada"],
+      ["Verde", "annie"],
+      ["Amarelo", "brenda"],
+      ["Preto", "grace"],
+      ["Branco", "mary"],
+    ],
+  },
 ];
 
 export default questionData;
