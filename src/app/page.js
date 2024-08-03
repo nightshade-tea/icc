582d887dc0662235dc0696816b50b0edfdd8e828
pedro friedrich <phfr24@inf.ufpr.br>
2024-08-03 00:42:43 -0300

    fix page padding

--- src/app/page.js
+++ src/app/page.js
@@ -3,13 +3,6 @@ import styles from "@/styles/page.module.css";
 export default function Index() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Preparado para descobrir qual mulher incrível da história da tecnologia você seria?<br/>
-          Desde Ada Lovelace, a primeira programadora, até as "computadoras" da NASA que levaram o homem à Lua, as mulheres têm revolucionado o mundo da computação.<br />
-          Vamos celebrar essas mentes brilhantes e ver com qual dessas pioneiras você mais se identifica! 💡🚀
-        </p>
-      </div>
       <div className={styles.grid}>
 
        <a
@@ -22,6 +15,13 @@ export default function Index() {
 
           <p>Pronto para começar? Vamos lá! 🎉🖥️</p>
         </a>
+      </div>
+      <div>
+        <p>
+          Preparado para descobrir qual mulher incrível da história da tecnologia você seria?<br/>
+          Desde Ada Lovelace, a primeira programadora, até as "computadoras" da NASA que levaram o homem à Lua, as mulheres têm revolucionado o mundo da computação.<br />
+          Vamos celebrar essas mentes brilhantes e ver com qual dessas pioneiras você mais se identifica! 💡🚀
+        </p>
       </div>
     </main>
   );
