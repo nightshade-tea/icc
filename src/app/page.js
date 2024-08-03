5ad02e592b48eaade17b27138c05f0f5f998583c
pedro friedrich <phfr24@inf.ufpr.br>
2024-08-03 01:04:34 -0300

    beautify index

--- src/app/page.js
+++ src/app/page.js
@@ -3,26 +3,28 @@ import styles from "@/styles/page.module.css";
 export default function Index() {
   return (
     <main className={styles.main}>
-      <div className={styles.grid}>
-
-       <a
-          href="./question/1"
-          className={styles.card}
-        >
-          <h2>
-            Descubra qual computeira você é 🌟👩‍💻<span>-&gt;</span>
-          </h2>
-
-          <p>Pronto para começar? Vamos lá! 🎉🖥️</p>
-        </a>
-      </div>
+      <h1 className={styles.title}>
+        Qual pioneira da computação mais combina com você?
+      </h1>
       <div>
-        <p>
+        <p className={styles.text}>
           Preparado para descobrir qual mulher incrível da história da tecnologia você seria?<br/>
           Desde Ada Lovelace, a primeira programadora, até as "computadoras" da NASA que levaram o homem à Lua, as mulheres têm revolucionado o mundo da computação.<br />
           Vamos celebrar essas mentes brilhantes e ver com qual dessas pioneiras você mais se identifica! 💡🚀
         </p>
       </div>
-    </main>
+      <div className={styles.grid}>
+       <a
+          href="./question/1"
+          className={styles.card}
+        >
+          <h2>
+            Descubra qual computeira você é <span>🌟👩‍💻-&gt;</span>
+          </h2>
+
+          <p>Pronto para começar? Vamos lá! 🎉🖥️</p>
+        </a>
+      </div>
+   </main>
   );
 }
