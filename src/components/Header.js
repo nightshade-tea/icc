f45db07e5814b7bf7bb0e8556c08f0b636bc7ed4
pedro friedrich <phfr24@inf.ufpr.br>
2024-08-02 22:47:15 -0300

    update index page and add header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,8 @@ import styles from "@/styles/header.module.css";
 export default function Header() {
   return (
     <header className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Os&nbsp;
-          <code className={styles.code}>Investidos</code>
-        </p>
+      <div className={styles.header}>
+        <h1>Qual pioneira da computação mais combina com você?</h1>
       </div>
     </header>
   );
