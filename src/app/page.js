19ae9eee2f768e6da8c3d8a0f824495b28d26a2c
pedro friedrich <phfr24@inf.ufpr.br>
2024-08-02 22:47:15 -0300

    update index page and add header

--- src/app/page.js
+++ src/app/page.js
@@ -3,15 +3,24 @@ import styles from "@/styles/page.module.css";
 export default function Index() {
   return (
     <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Preparado para descobrir qual mulher incrível da história da tecnologia você seria?<br/>
+          Desde Ada Lovelace, a primeira programadora, até as "computadoras" da NASA que levaram o homem à Lua, as mulheres têm revolucionado o mundo da computação.<br />
+          Vamos celebrar essas mentes brilhantes e ver com qual dessas pioneiras você mais se identifica! 💡🚀
+        </p>
+      </div>
       <div className={styles.grid}>
-        <a
+
+       <a
           href="./question/1"
           className={styles.card}
         >
           <h2>
-            Iniciar quiz <span>-&gt;</span>
+            Descubra qual computeira você é 🌟👩‍💻<span>-&gt;</span>
           </h2>
-          <p>Descubra qual computeira você é</p>
+
+          <p>Pronto para começar? Vamos lá! 🎉🖥️</p>
         </a>
       </div>
     </main>
